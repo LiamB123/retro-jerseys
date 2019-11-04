8d041b45c9239ee72b1ecebc35955d55e6b3d302
Ubuntu <ubuntu@ip-172-31-90-29.ec2.internal>
2019-11-04 15:10:36 +0000

    added in accounts functionality

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,7 @@
-  
 from django.conf.urls import url, include
 from accounts.views import logout, login, registration, user_profile
 from accounts import url_reset
+
 
 urlpatterns = [
     url(r'^logout/', logout, name="logout"),
--- retro_jerseys/settings.py
+++ retro_jerseys/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = '_k=dnppvi41mv!_lcs1$$t08w!t8jdeah!gzp0cn(76ztjo9bs'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get('C9_HOSTNAME')]
+ALLOWED_HOSTS = [os.environ.get('C9_HOSTNAME'),
+                "34912d2a20af41968452a084c123d29a.vfs.cloud9.us-east-1.amazonaws.com",]
 
 
 # Application definition
@@ -37,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_forms_bootstrap',
+    'accounts',
+    
 ]
 
 MIDDLEWARE = [
@@ -54,7 +58,7 @@ ROOT_URLCONF = 'retro_jerseys.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -100,6 +104,11 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+AUTHENTICATION_BACKENDS= [
+    'django.contrib.auth.backends.ModelBackend',
+    'accounts.backends.CaseInsensitiveAuth']
+
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
@@ -118,3 +127,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
